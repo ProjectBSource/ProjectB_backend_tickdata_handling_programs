3f041954166f4abb11eb56cb6ae9220d351ca404
PatrickFung <patrick.fung@team-c.com>
2023-04-19 16:51:19 +0800

    format HKEX future tickdata

---
+++ HKEX_FUTURE_TickData_Formatting_version_20220330_to_now.java
@@ -0,0 +1,115 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Calendar;
+
+/*
+ * java HKEX_FUTURE_TickData_Formatting_version_20220330_to_now [day file path] [night file path] [output file path]
+ */
+
+public class HKEX_FUTURE_TickData_Formatting_version_20220330_to_now {
+
+    static File tickdata_tr = null;
+    static FileReader fr_tr = null;
+    static BufferedReader br_tr = null;
+    static String line_tr = null;
+    static String last_line_tr_before_switch = null;
+
+    static File tickdata_tr_aht = null;
+    static FileReader fr_tr_aht = null;
+    static BufferedReader br_tr_aht = null;
+    static String line_tr_aht = null;
+    static String last_line_tr_aht = null;
+    static String last_line_tr_aht_before_switch = null;
+
+    static String outputFilepath = null;
+
+    public static void main(String args[]) throws Exception{
+        tickdata_tr = new File (args[0]);
+        fr_tr = new FileReader(tickdata_tr);
+        br_tr = new BufferedReader(fr_tr);
+        line_tr = null;
+
+        tickdata_tr_aht = new File (args[1]);
+        fr_tr_aht = new FileReader(tickdata_tr_aht);
+        br_tr_aht = new BufferedReader(fr_tr_aht);
+        line_tr_aht = null;
+
+        outputFilepath = args[2];
+
+        readFile(1);
+        System.out.println("finished");
+    }
+
+    private static void readFile(int aht_or_not) throws Exception{
+        StringBuilder formated_content = new StringBuilder();
+        if(aht_or_not==1){
+            if(last_line_tr_before_switch!=null){
+                formated_content.append(last_line_tr_before_switch);
+                formated_content.append("\n");
+                last_line_tr_before_switch = null;
+            }
+            Date begin_yyyyMMdd = null;
+            while((line_tr = br_tr.readLine())!=null){
+                if(begin_yyyyMMdd == null){
+                    System.out.println(line_tr);
+                    begin_yyyyMMdd = new SimpleDateFormat("yyyyMMdd").parse(line_tr.substring(33, 41));
+                }
+                Date curr_yyyyMMdd = new SimpleDateFormat("yyyyMMdd").parse(line_tr.substring(33, 41));
+                if(curr_yyyyMMdd.equals(begin_yyyyMMdd)==false){
+                    last_line_tr_before_switch = line_tr;
+                    writeFile(formated_content);
+                    readFile(2);
+                    break;
+                }
+                formated_content.append(line_tr);
+                formated_content.append("\n");
+            }
+            aht_or_not = -1;
+            fr_tr.close();
+            br_tr.close();
+        }
+        else if(aht_or_not==2){
+            if(last_line_tr_aht_before_switch!=null){
+                formated_content.append(last_line_tr_aht_before_switch);
+                formated_content.append("\n");
+                last_line_tr_aht_before_switch = null;
+            }
+            Date end_yyyyMmddkk = null;
+            while((line_tr_aht = br_tr_aht.readLine())!=null){
+                if(end_yyyyMmddkk == null){
+                    System.out.println(line_tr_aht);
+                    end_yyyyMmddkk = new SimpleDateFormat("yyyyMMddkk").parse(line_tr_aht.substring(33, 43));
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTime(end_yyyyMmddkk);
+                    calendar.add(Calendar.HOUR_OF_DAY, 12);
+                    end_yyyyMmddkk = calendar.getTime();
+                }
+                Date curr_yyyyMMddkk = new SimpleDateFormat("yyyyMMddkk").parse(line_tr_aht.substring(33, 43));
+                if(curr_yyyyMMddkk.after(end_yyyyMmddkk)){
+                    last_line_tr_aht_before_switch = line_tr_aht;
+                    writeFile(formated_content);
+                    readFile(1);
+                    break;
+                }
+                formated_content.append(line_tr_aht);
+                formated_content.append("\n");
+            }
+            aht_or_not = -1;
+            fr_tr_aht.close();
+            br_tr_aht.close();
+        }
+    }
+
+    private static void writeFile(StringBuilder content) throws Exception{
+        FileWriter fw = new FileWriter(outputFilepath, true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(content.toString());
+        bw.close();
+        fw.close();
+    }
+}
